1d337ab5a1cd49238aa8a13f0a8e6d91cc967627
mj <phenomenal_ab@linuxmail.org>
2020-11-14 12:34:00 +0530

    add OrdersChart Component

--- src/OrdersChart.js
+++ src/OrdersChart.js
@@ -1,43 +1,27 @@
 import React from "react";
 import Chart from "react-google-charts";
 const OrdersChart = (props) => {
-  //   const { data } = props;
+  const { data } = props;
+  const chartData = [["No. of Orders", "Customers Count"]];
 
-  //   Chart.charts.load("current", { packages: ["corechart"] });
-  //   Chart.charts.setOnLoadCallback(drawChart);
-
-  //   const chartData = [["No of Orders", "Customer Count"]];
-  //   for (const key in data) {
-  //     chartData.push([key, data[key]]);
-  //   }
-
-  //   function drawChart() {
-  //     const data = Chart.visualization.arrayToDataTable(chartData);
-  //     const options = {
-  //       title: "Customer Distribution ",
-  //       width: 450,
-  //       height: 300,
-  //       fontSize: 25,
-  //       // backgroundColor: "limegreen",
-  //       bar: { groupWidth: "50%" },
-  //       legend: { position: "none" },
-  //     };
-  //     const chart = new Chart.visualization.ColumnChart(
-  //       document.getElementById("columnchart_values")
-  //     );
-  //     chart.draw(data, options);
-  //   }
+  for (const key in data) {
+    chartData.push([key, data[key]]);
+  }
 
   return (
     <div className="col-md-6">
-      <div
-        id="columnchart_values"
-        style={{
-          width: "400px",
-          height: " 300px",
+      <Chart
+        width={"650px"}
+        height={"350px"}
+        chartType="PieChart"
+        loader={<div>Loading Charts</div>}
+        data={chartData}
+        options={{
+          title: "Orders Distribution",
         }}
-      ></div>
+      />
     </div>
   );
 };
+
 export default OrdersChart;
